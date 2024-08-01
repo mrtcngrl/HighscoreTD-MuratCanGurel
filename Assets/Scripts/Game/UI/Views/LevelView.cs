50516019027955deabb9dfce417668c0c5433862
mrtcngrl <56045489+mrtcngrl@users.noreply.github.com>
2024-08-01 19:32:21 +0300

    popup added

--- Assets/Scripts/Game/UI/Views/LevelView.cs
+++ Assets/Scripts/Game/UI/Views/LevelView.cs
@@ -9,18 +9,20 @@ namespace Scripts.Game.UI.Views
     {
         [SerializeField] private TextMeshProUGUI _coinText;
         [SerializeField] private TextMeshProUGUI _scoreText;
-        [SerializeField] private GameObject _scorePopup;
+        [SerializeField] private ScorePopup _scorePopup;
         [Inject]
         private void OnInject()
         {
             GameConstants.CoinAmountChanged += OnCoinAmountChanged;
             GameConstants.ScoreChaged += OnScoreChanged;
+            GameConstants.OnSessionEnd += OnSessionEnd;
         }
 
         private void OnDestroy()
         {
             GameConstants.CoinAmountChanged -= OnCoinAmountChanged;
             GameConstants.ScoreChaged -= OnScoreChanged;
+            GameConstants.OnSessionEnd -= OnSessionEnd;
         }
 
         private void OnCoinAmountChanged(int amount)
@@ -31,6 +33,11 @@ namespace Scripts.Game.UI.Views
         private void OnScoreChanged(int score)
         {
             _scoreText.text = score.ToString();
+        }
+
+        private void OnSessionEnd()
+        {
+            _scorePopup.ShowPopup();
         }
     }
 }
